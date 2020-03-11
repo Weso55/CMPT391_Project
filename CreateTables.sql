8c9bdb77544028f02ea1ea1ca311814f7cdb0f0c
Weso55 <olsonw7@mymacewan.ca>
2020-03-11 03:27:40 -0600

    Update CreateTables.sql

--- CreateTables.sql
+++ CreateTables.sql
@@ -102,4 +102,12 @@ CREATE TABLE [Enrolled] (
   PRIMARY KEY ([Student_ID], [Couse_ID], [Semester], [Year])
 );
 
+--------------
+                                                      
+                                                      
+CREATE VIEW detail AS
+SELECT Courses.Course_ID AS Class_ID, Courses.Name AS C_Name, Courses.Section_ID AS S_ID,Instructors.Name AS I_name,Department.Name AS D_name,Courses.Capacity AS cap   
+FROM Courses,Instructors,Department  
+WHERE Courses.Department_ID = Department.Department_ID AND Instructors.Department_ID = Department.Department_ID
+                                                     
                                                                 
