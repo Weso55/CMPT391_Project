dbc6e2015d44762b83f02ab1322f541a930f61bc
Nathan Wong <wongn36@mymacewan.ca>
2020-04-05 04:02:38 -0600

    changed detail view into a proper materialized (indexed) view
    its called an indexed view in sql server
    no clue if this is actually faster or not, but hey

--- CreateView.sql
+++ CreateView.sql
@@ -1,4 +1,11 @@
-CREATE OR ALTER VIEW detail AS
-SELECT Courses.Course_ID AS Class_ID, Courses.Name AS C_Name, Courses.Section_ID AS S_ID,Instructors.Name AS I_name,Courses.Semester AS Semester,Courses.Year AS Year,Department.Name AS D_name,Courses.Capacity AS cap   
-FROM Courses,Instructors,Department  
+GO
+CREATE OR ALTER VIEW detail
+WITH SCHEMABINDING
+AS
+SELECT Courses.Course_ID AS Class_ID, Courses.Name AS C_Name, Courses.Section_ID AS S_ID, Instructors.Name AS I_name, Courses.Semester AS Semester, Courses.Year AS Year, Department.Name AS D_name, Courses.Capacity AS cap   
+FROM dbo.Courses, dbo.Instructors, dbo.Department  
 WHERE Courses.Instructor_ID = Instructors.Instructor_ID AND Instructors.Department_ID = Department.Department_ID;
+
+GO
+CREATE UNIQUE CLUSTERED INDEX index1
+ON detail(Class_ID, S_ID, Semester, Year)
