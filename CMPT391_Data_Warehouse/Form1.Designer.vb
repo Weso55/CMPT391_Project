32aed1b1a9053265c6fa368fd3a7917d3311c898
Wesley Olson <weslMOlso@live.com>
2020-04-06 11:38:26 -0600

    Completed (I think) the datawarehouse applicaiton

--- CMPT391_Data_Warehouse/Form1.Designer.vb
+++ CMPT391_Data_Warehouse/Form1.Designer.vb
@@ -24,19 +24,28 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.GoButton = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.DepartmentComboBox = New System.Windows.Forms.ComboBox()
         Me.Label4 = New System.Windows.Forms.Label()
-        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
-        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.SemesterComboBox = New System.Windows.Forms.ComboBox()
+        Me.YearComboBox = New System.Windows.Forms.ComboBox()
         Me.Label3 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.InstructorTextBox = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.FacultyComboBox = New System.Windows.Forms.ComboBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'GoButton
         '
-        Me.GoButton.Location = New System.Drawing.Point(319, 219)
+        Me.GoButton.Location = New System.Drawing.Point(750, 178)
         Me.GoButton.Name = "GoButton"
         Me.GoButton.Size = New System.Drawing.Size(95, 38)
         Me.GoButton.TabIndex = 1
@@ -46,49 +55,52 @@ Partial Class Form1
         'Label1
         '
         Me.Label1.AutoSize = True
-        Me.Label1.Location = New System.Drawing.Point(325, 96)
+        Me.Label1.Location = New System.Drawing.Point(379, 29)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(57, 13)
+        Me.Label1.Size = New System.Drawing.Size(135, 13)
         Me.Label1.TabIndex = 2
-        Me.Label1.Text = "Find Total:"
+        Me.Label1.Text = "Find Total Courses Taught:"
         '
-        'ComboBox2
+        'DepartmentComboBox
         '
-        Me.ComboBox2.FormattingEnabled = True
-        Me.ComboBox2.Location = New System.Drawing.Point(94, 162)
-        Me.ComboBox2.Name = "ComboBox2"
-        Me.ComboBox2.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox2.TabIndex = 6
+        Me.DepartmentComboBox.FormattingEnabled = True
+        Me.DepartmentComboBox.Items.AddRange(New Object() {"Department of Animal Science", "Department of Anthropology", "Department of Geography", "Department of Physical Education", "Department of Plant Science", "Department of Sciences and Forestry"})
+        Me.DepartmentComboBox.Location = New System.Drawing.Point(75, 146)
+        Me.DepartmentComboBox.Name = "DepartmentComboBox"
+        Me.DepartmentComboBox.Size = New System.Drawing.Size(193, 21)
+        Me.DepartmentComboBox.TabIndex = 6
         '
         'Label4
         '
         Me.Label4.AutoSize = True
-        Me.Label4.Location = New System.Drawing.Point(257, 165)
+        Me.Label4.Location = New System.Drawing.Point(29, 149)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(16, 13)
         Me.Label4.TabIndex = 7
         Me.Label4.Text = "In"
         '
-        'ComboBox3
+        'SemesterComboBox
         '
-        Me.ComboBox3.FormattingEnabled = True
-        Me.ComboBox3.Location = New System.Drawing.Point(309, 162)
-        Me.ComboBox3.Name = "ComboBox3"
-        Me.ComboBox3.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox3.TabIndex = 8
+        Me.SemesterComboBox.FormattingEnabled = True
+        Me.SemesterComboBox.Items.AddRange(New Object() {"Fall", "Winter", "Spring", "Summer"})
+        Me.SemesterComboBox.Location = New System.Drawing.Point(382, 146)
+        Me.SemesterComboBox.Name = "SemesterComboBox"
+        Me.SemesterComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.SemesterComboBox.TabIndex = 8
         '
-        'ComboBox4
+        'YearComboBox
         '
-        Me.ComboBox4.FormattingEnabled = True
-        Me.ComboBox4.Location = New System.Drawing.Point(456, 162)
-        Me.ComboBox4.Name = "ComboBox4"
-        Me.ComboBox4.Size = New System.Drawing.Size(121, 21)
-        Me.ComboBox4.TabIndex = 9
+        Me.YearComboBox.FormattingEnabled = True
+        Me.YearComboBox.Items.AddRange(New Object() {"2020", "2021", "2022"})
+        Me.YearComboBox.Location = New System.Drawing.Point(581, 149)
+        Me.YearComboBox.Name = "YearComboBox"
+        Me.YearComboBox.Size = New System.Drawing.Size(121, 21)
+        Me.YearComboBox.TabIndex = 9
         '
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(343, 142)
+        Me.Label3.Location = New System.Drawing.Point(421, 130)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(51, 13)
         Me.Label3.TabIndex = 10
@@ -97,7 +109,7 @@ Partial Class Form1
         'Label5
         '
         Me.Label5.AutoSize = True
-        Me.Label5.Location = New System.Drawing.Point(498, 142)
+        Me.Label5.Location = New System.Drawing.Point(624, 130)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(29, 13)
         Me.Label5.TabIndex = 11
@@ -108,21 +120,109 @@ Partial Class Form1
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Location = New System.Drawing.Point(32, 301)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(788, 358)
+        Me.DataGridView1.Size = New System.Drawing.Size(802, 390)
         Me.DataGridView1.TabIndex = 12
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(163, 191)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(18, 13)
+        Me.Label2.TabIndex = 13
+        Me.Label2.Text = "Or"
+        '
+        'InstructorTextBox
+        '
+        Me.InstructorTextBox.Location = New System.Drawing.Point(75, 235)
+        Me.InstructorTextBox.Name = "InstructorTextBox"
+        Me.InstructorTextBox.Size = New System.Drawing.Size(193, 20)
+        Me.InstructorTextBox.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(134, 219)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(65, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "Instructor ID"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(137, 130)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(62, 13)
+        Me.Label7.TabIndex = 16
+        Me.Label7.Text = "Department"
+        '
+        'FacultyComboBox
+        '
+        Me.FacultyComboBox.FormattingEnabled = True
+        Me.FacultyComboBox.Items.AddRange(New Object() {"Faculty of Agriculture and Food Sciences", "Faculty of Forestry, Geography and Geomatics", "Faculty of Educational Sciences", "Faculty of Social Sciences"})
+        Me.FacultyComboBox.Location = New System.Drawing.Point(75, 66)
+        Me.FacultyComboBox.Name = "FacultyComboBox"
+        Me.FacultyComboBox.Size = New System.Drawing.Size(193, 21)
+        Me.FacultyComboBox.TabIndex = 17
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(165, 102)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(18, 13)
+        Me.Label8.TabIndex = 18
+        Me.Label8.Text = "Or"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(137, 50)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(41, 13)
+        Me.Label9.TabIndex = 19
+        Me.Label9.Text = "Faculty"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(295, 154)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(39, 13)
+        Me.Label10.TabIndex = 20
+        Me.Label10.Text = "Where"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(527, 154)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(25, 13)
+        Me.Label11.TabIndex = 21
+        Me.Label11.Text = "and"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(879, 750)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.FacultyComboBox)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.InstructorTextBox)
+        Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.DataGridView1)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.ComboBox4)
-        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.YearComboBox)
+        Me.Controls.Add(Me.SemesterComboBox)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.DepartmentComboBox)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.GoButton)
         Me.Name = "Form1"
@@ -134,11 +234,20 @@ Partial Class Form1
     End Sub
     Friend WithEvents GoButton As Button
     Friend WithEvents Label1 As Label
-    Friend WithEvents ComboBox2 As ComboBox
+    Friend WithEvents DepartmentComboBox As ComboBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents ComboBox3 As ComboBox
-    Friend WithEvents ComboBox4 As ComboBox
+    Friend WithEvents SemesterComboBox As ComboBox
+    Friend WithEvents YearComboBox As ComboBox
     Friend WithEvents Label3 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Label2 As Label
+    Friend WithEvents InstructorTextBox As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents FacultyComboBox As ComboBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
 End Class
--- CMPT391_Data_Warehouse/Form1.vb
+++ CMPT391_Data_Warehouse/Form1.vb
@@ -1,13 +1,72 @@
 ﻿Imports System.Data.SqlClient
 Public Class Form1
-    Private con_string As String
-    Private Sub Button1_Click(sender As Object, e As EventArgs) 
-        Dim d_form As New Data_Generation
-        con_string = "Data Source=DESKTOP-IRRDDVT\SQLEXPRESS;Initial Catalog=CMPT391;Integrated Security=True"
-        d_form.Startup(con_string)
-        d_form.ShowDialog()
+    Private user_id As Integer
+    Private con As SqlConnection
+    Private cmd As SqlCommand
+    Private adpt As SqlDataAdapter
+    Private Sub GoButton_Click(sender As Object, e As EventArgs) Handles GoButton.Click
+        Try
+            'open new connection'
+            Dim con_string = "Data Source=DESKTOP-IRRDDVT\SQLEXPRESS;Initial Catalog=CMPT391_Data_Warehouse;Integrated Security=True"
+            con = New SqlConnection(con_string)
+            cmd = New SqlCommand
+            'assign parameters for stored procedure
+            Dim params(4) As SqlParameter
+
+            params(0) = New SqlParameter("@I_ID", SqlDbType.VarChar)
+            If Not InstructorTextBox.Text = "" Then
+                'where_string = where_string & " Name=" & CourseTextBox.Text & " "
+                params(0).Value = InstructorTextBox.Text
+            Else
+                params(0).Value = "null"
+            End If
+
+            params(1) = New SqlParameter("@faculty", SqlDbType.VarChar)
+            If Not FacultyComboBox.SelectedIndex = -1 Then
+                params(1).Value = FacultyComboBox.Text
+            Else
+                params(1).Value = "null"
+            End If
+
+            params(2) = New SqlParameter("@department", SqlDbType.VarChar)
+            If Not DepartmentComboBox.SelectedIndex = -1 Then
+                params(2).Value = DepartmentComboBox.Text
+            Else
+                params(2).Value = "null"
+            End If
+
+            params(3) = New SqlParameter("@semester", SqlDbType.VarChar)
+            If Not SemesterComboBox.SelectedIndex = -1 Then
+                params(3).Value = SemesterComboBox.Text
+            Else
+                params(3).Value = "null"
+            End If
+
+            params(4) = New SqlParameter("@year", SqlDbType.VarChar)
+            If Not YearComboBox.SelectedIndex = -1 Then
+                params(4).Value = YearComboBox.Text
+            Else
+                params(4).Value = "null"
+            End If
+
+            MessageBox.Show("Values: " + params(0).Value.ToString() + ", " + params(1).Value.ToString() + ", " + params(2).Value.ToString() + ", " + params(3).Value.ToString() + ", " + params(4).Value.ToString() + ", ")
+            'Assign variables for connection
+            cmd.Connection = con
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.CommandText = "Course_Value"
+            cmd.Parameters.AddRange(params)
+
+            adpt = New SqlDataAdapter
+            adpt.SelectCommand = cmd
+
+
+            'fill table with enrolled details'
+            Dim dt As DataTable = New DataTable
+            adpt.Fill(dt)
+            DataGridView1.DataSource = dt
+
+        Catch ex As Exception
+            MsgBox("Error with Filling Course_Total data table: " & ex.Message & " ")
+        End Try
     End Sub
-
-
-
 End Class
